0da9e553d9abd21bfc7c23aa9775437eb9aa0da4
hnord-vdx <henrik.nordgren@vaerdex.ch>
2022-02-24 23:53:40 +0100

    Update ethereum_example.dart

--- example/lib/ethereum_example.dart
+++ example/lib/ethereum_example.dart
@@ -1,12 +1,8 @@
-import 'dart:convert';
-import 'dart:typed_data';
-
 import 'package:convert/convert.dart';
 import 'package:flutter/material.dart';
 import 'package:trust_wallet_core/flutter_trust_wallet_core.dart';
 import 'package:trust_wallet_core/trust_wallet_core_ffi.dart';
 import 'package:trust_wallet_core_example/base_example.dart';
-import 'package:trust_wallet_core/protobuf/Ethereum.pb.dart' as Ethereum;
 
 class EthereumExample extends BaseExample {
   final HDWallet wallet;
@@ -22,16 +18,24 @@ class _EthereumExampleState extends BaseExampleState<EthereumExample> {
   void initState() {
     super.initState();
     logger.d("mnemonic: ${widget.wallet.mnemonic()}");
-    logger.d("address: ${widget.wallet.getAddressForCoin(TWCoinType.TWCoinTypeEthereum)}");
-    final publicKey = widget.wallet.getKeyForCoin(TWCoinType.TWCoinTypeEthereum).getPublicKeySecp256k1(false);
-    AnyAddress anyAddress = AnyAddress.createWithPublicKey(publicKey, TWCoinType.TWCoinTypeEthereum);
+    logger.d(
+        "address: ${widget.wallet.getAddressForCoin(TWCoinType.TWCoinTypeEthereum)}");
+    final publicKey = widget.wallet
+        .getKeyForCoin(TWCoinType.TWCoinTypeEthereum)
+        .getPublicKeySecp256k1(false);
+    AnyAddress anyAddress = AnyAddress.createWithPublicKey(
+        publicKey, TWCoinType.TWCoinTypeEthereum);
     logger.d("address from any address: ${anyAddress.description()}");
     print(widget.wallet.mnemonic());
-    String privateKeyhex = hex.encode(widget.wallet.getKeyForCoin(TWCoinType.TWCoinTypeEthereum).data());
+    String privateKeyhex = hex.encode(
+        widget.wallet.getKeyForCoin(TWCoinType.TWCoinTypeEthereum).data());
     logger.d("privateKeyhex: $privateKeyhex");
     logger.d("seed = ${hex.encode(widget.wallet.seed())}");
-    final keystore = StoredKey.importPrivateKey(widget.wallet.getKeyForCoin(TWCoinType.TWCoinTypeEthereum).data(), "name", "password", TWCoinType.TWCoinTypeEthereum);
+    final keystore = StoredKey.importPrivateKey(
+        widget.wallet.getKeyForCoin(TWCoinType.TWCoinTypeEthereum).data(),
+        "name",
+        "password",
+        TWCoinType.TWCoinTypeEthereum);
     logger.d("keystore: ${keystore?.exportJson()}");
-
   }
 }
